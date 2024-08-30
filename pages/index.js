770c0d5d985c7ef773c0702d596afe156731335f
Gabriel Leal <143210462+Bruukii@users.noreply.github.com>
2024-08-30 18:53:54 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>te amo momo ❤️</h1>
+function Home() {
+  return <h1>te amo momo ❤️</h1>;
 }
 
+function test() {
+  console.log("teste");
+}
+function test2() {
+  console.log("indetação errada");
+}
 export default Home;
